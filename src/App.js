f399686b34f9105441dccc7e9521a9b674313d45
mk-hasan <mkhasan6891@gmail.com>
2022-04-03 21:50:17 +0600

    components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <Home></Home>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Trust-Site</h2>
+            <p>Never miss user experience</p>
+        </div>
+    );
+};
+
+export default Home;
