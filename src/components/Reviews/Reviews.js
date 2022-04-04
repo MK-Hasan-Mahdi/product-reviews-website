cf0645970afbc78e1eee2d34a29779b92ea0db37
mk-hasan <mkhasan6891@gmail.com>
2022-04-04 14:20:00 +0600

    navbar bring to center

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -5,8 +5,7 @@ const Reviews = (props) => {
     const { picture, name, clientReview, rating } = props.review;
     return (
         <div className='single-review'>
-            <h5>Clinet Reviews</h5>
-            <img src={picture} height={50} width={50} alt="" />
+            <img src={picture} height={100} width={100} alt="" />
             <h6>Name: {name} </h6>
             <p> {clientReview} </p>
             <span> {rating} </span>
